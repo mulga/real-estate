8b48393b3c5feab4dd43d21528bb913cb06e71a9
Mehmet Ulga <mehmetulga@gmail.com>
2022-12-14 12:02:33 +0100

    v2

--- main.js
+++ main.js
@@ -1,128 +1,18 @@
-// let listItems = {
-//   data: [
-//     {
-//       id: 1,
-//       image: "./assets/h1.jpeg",
-//       title: "Grand Family House",
-//       location: "Ohio St. South Gate, California",
-//       category: "house",
-//       price: 350,
-//       star: 4,
-//     },
-//     {
-//       id: 2,
-//       image: "./assets/h2.jpeg",
-//       title: "The Elements",
-//       location: "Jakarta Selatan, DKI Jakarta",
-//       category: "apartment",
-//       price: 620,
-//       star: 4,
-//     },
-//     {
-//       id: 3,
-//       image: "./assets/h3.jpeg",
-//       title: "Triraksa Village 2",
-//       location: "Tangerang, Banten",
-//       category: "villa",
-//       price: 682,
-//       star: 4,
-//     },
-//     {
-//       id: 4,
-//       image: "./assets/h4.jpeg",
-//       title: "Nuvasa Bay",
-//       location: "Batam, Kepulauan Riau",
-//       category: "villa",
-//       price: 682,
-//       star: 4,
-//     },
-//   ],
-// };
+import { homeList } from "./homeData.js";
 
-let listItems = [
-  {
-    id: 1,
-    image: "./assets/h1.jpeg",
-    title: "Grand Family House",
-    location: "Ohio St. South Gate, California",
-    category: "house",
-    price: 350,
-    star: 4,
-  },
-  {
-    id: 2,
-    image: "./assets/h2.jpeg",
-    title: "The Elements",
-    location: "Jakarta Selatan, DKI Jakarta",
-    category: "apartment",
-    price: 620,
-    star: 4,
-  },
-  {
-    id: 3,
-    image: "./assets/h3.jpeg",
-    title: "Triraksa Village 2",
-    location: "Tangerang, Banten",
-    category: "villa",
-    price: 682,
-    star: 4,
-  },
-  {
-    id: 4,
-    image: "./assets/h4.jpeg",
-    title: "Nuvasa Bay",
-    location: "Batam, Kepulauan Riau",
-    category: "villa",
-    price: 682,
-    star: 4,
-  },
-]
-//
-// for (let i = 0; i < listItems.length; i++) {
-//   let listingWrapper = document.getElementById("listingWrapper").innerHTML = `
-//
-//    <div class="listing-card">
-//               <div class="listing-card-image img1">
-//                 <img src="${[i].image}" alt="" />
-//               </div>
-//               <div class="card-content">
-//                 <h5 class="h5-bold">${[i].title}</h5>
-//                 <div
-//                   class="location d-flex justify-content-between align-item-center"
-//                 >
-//                   <p class="body-medium">
-//                     <span><i class="fa-solid fa-location-dot"></i></span> ${[i].location}
-//                   </p>
-//                 </div>
-//
-//                 <div
-//                   class="price d-flex justify-content-between align-item-center"
-//                 >
-//                   <h5 class="h5-bold"><span>$</span>${[i].price}</h5>
-//                   <p class="body-semibold">
-//                     <span><i class="fa-solid fa-star"></i></span
-//                     ><span> ${[i].star} </span>
-//                   </p>
-//                 </div>
-//               </div>
-//             </div>
-//
-//   `
-// }
-
-
-
-for (let i of listItems) {
-  console.log(i["title"]);
+for (let item of homeList) {
   // Create element
   let listingCard = document.createElement("div");
-  listingCard.classList.add("listing-card", "i.category");
-
+  listingCard.classList.add("listing-card");
+  // listingCard.filterV = item.homeType.replace(/_/g, " ").toLowerCase();
+  listingCard
+    .setAttribute("data-filter", item.homeType)
+    .replace(/_/g, " ")
+    .toLowerCase();
   let listingCardImage = document.createElement("div");
   listingCardImage.classList.add("listing-card-image");
-
   let image = document.createElement("img");
-  image.setAttribute("src", i.image);
+  image.setAttribute("src", item.photos[0]);
   listingCard.appendChild(listingCardImage);
   listingCardImage.appendChild(image);
   document.getElementById("listingWrapper").appendChild(listingCard);
@@ -133,12 +23,57 @@ for (let i of listItems) {
   // card title
   let cardTitle = document.createElement("h5");
   cardTitle.classList.add("h5-bold", "card-title");
-  cardTitle.innerText = i.title;
+  cardTitle.innerText = item.address.city;
   cardContent.appendChild(cardTitle);
   ///
   // location
-  let location = document.createElement("div")
-  location.classList.add("location")
+  let location = document.createElement("div");
+  location.classList.add("location");
+  cardContent.appendChild(location);
+  // location icon
+  let locationIcon = document.createElement("span");
+  locationIcon.classList.add("location-icon");
+  location.appendChild(locationIcon);
+  // location icon
+  let locationIconI = document.createElement("i");
+  locationIconI.classList.add("fa-solid");
+  locationIconI.classList.add("fa-location-dot");
+  locationIcon.appendChild(locationIconI);
+  // location text
+  let locationText = document.createElement("p");
+  locationText.classList.add("body-medium", "location-text");
+  locationText.innerText = item.address.streetAddress;
+  location.appendChild(locationText);
+  // price
+  let price = document.createElement("div");
+  price.classList.add("price");
+  price.innerText = item.currency + " " + item.price;
+  cardContent.appendChild(price);
 
+  // homestatus
+  let homeStatus = document.createElement("div");
+  homeStatus.classList.add("home-status");
+  homeStatus.innerText = item.homeStatus.replace(/_/g, " ");
+  price.appendChild(homeStatus);
+}
 
+// filter
+let filterBtn = document.querySelectorAll(".filter-btn");
+for (let i = 1; i < filterBtn.length; i++) {
+  filterBtn[i].addEventListener("click", function () {
+    let filterValue = filterBtn[i].innerText.toLowerCase();
+    let listingCard = document.querySelectorAll(".listing-card");
+    console.log("listing card: " + listingCard);
+    console.log("filter value: " + filterValue);
+
+    // for (let item of listingCard) {
+    //   if (item.children[1].children[0].innerText == filterValue) {
+    //     item.classList = "block";
+    //   } else if (filterValue == "All") {
+    //     item.style.display = "block";
+    //   } else {
+    //     item.style.display = "none";
+    //   }
+    // }
+  });
 }
